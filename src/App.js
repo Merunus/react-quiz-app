4946be2933fd03445807748714bb2026e41633a2
Dmitry <95978902+merunus@users.noreply.github.com>
2022-06-02 00:38:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
         <div className="app" style = {{backgroundImage:"url(./ques1.png"}}>
             <Header />
           <Routes>
-              <Route  path = "/" element = {<Home name = {name} setName = {setName} fetchQuestions = {fetchQuestions} />}/>
+              <Route  path = "/react-quiz" element = {<Home name = {name} setName = {setName} fetchQuestions = {fetchQuestions} />}/>
               <Route path = "/quiz" element = {<Quiz name = {name} questions = {questions} score = {score} setScore = {setScore} setQuestions = {setQuestions} />}/>
               <Route path = "/result" element = {<Result score = {score} name = {name} />}/>
                   
